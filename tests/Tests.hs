37d53c0af35e101f206bd0fd7f0fef7e4b0ef3fc
Tom Hunger <tehunger@gmail.com>
2015-12-09 19:40:18 +0000

    Various.

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -4,8 +4,9 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import qualified System.Linux.Seccomp as S
 import Foreign.Ptr (nullPtr)
-import System.Posix.Process (forkProcess, getProcessStatus)
+import System.Posix.Process (forkProcess, getProcessStatus, ProcessStatus(..))
 import System.IO (openFile, IOMode(..))
+import System.Posix.Signals (sigSYS)
 
 main :: IO ()
 main = defaultMain unitTests
@@ -15,19 +16,19 @@ unitTests = testGroup "Unit tests"
   [ testCase "init" $ do
         ctx <- S.seccomp_init S.SCMP_ACT_KILL
         if ctx /= nullPtr then return () else assertFailure "seccomp_init returned 0"
-  , testFork
+  , testCase "kill on any open"  $ do
+        assertTerminated killOnAnyOpen
   ]
 
-testFork :: TestTree
-testFork = testCase "kill fork" $ do
-    pid <- forkProcess tc
-    r <- getProcessStatus True False pid
-    print r
+killOnAnyOpen = do
+    ctx <- S.seccomp_init S.SCMP_ACT_KILL
+    _ <- S.seccomp_rule_add_array ctx S.SCMP_ACT_ALLOW S.SCopen []
+    S.seccomp_load ctx
+    openFile "/dev/null" ReadMode
     return ()
-  where
-    tc = do
-        ctx <- S.seccomp_init S.SCMP_ACT_KILL
-        _ <- S.seccomp_rule_add_array ctx S.SCMP_ACT_ALLOW S.SCopen []
-        S.seccomp_load ctx
-        openFile "/dev/null" ReadMode
-        return ()
+
+assertTerminated :: IO () -> Assertion
+assertTerminated f = do
+    pid <- forkProcess f
+    result <- getProcessStatus True False pid
+    assertBool "didn't terminate" (result == Just (Terminated sigSYS False))
